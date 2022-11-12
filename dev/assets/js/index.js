cc715756ad1417b7ca8a288ddad3e6e8caba6fde
Vyacheslav <parselian.study@yandex.ru>
2022-11-12 20:37:08 +0300

    Init brands slider

--- dev/assets/js/index.js
+++ dev/assets/js/index.js
@@ -1,1 +1,40 @@
 'use-strict'
+
+document.addEventListener('DOMContentLoaded', () => {
+  const burgerMenuContent = document.querySelector('.burger-menu__content'),
+    burgerBtns = document.querySelectorAll('.burger-btn'),
+    burgerMenuNavLinks = document.querySelectorAll('.burger-menu__nav-link')
+
+  const burgerMenuInit = (target) => {
+    if ( !!target.closest('.burger-btn')) {
+      burgerBtns.forEach(btn => btn.classList.toggle('burger-btn_active'))
+      burgerMenuContent.classList.toggle('burger-menu__content_visible')
+    } else if (!target.closest('.burger-menu__bar') && !target.closest('.burger-btn')) {
+      burgerBtns.forEach(btn => btn.classList.remove('burger-btn_active'))
+      burgerMenuContent.classList.remove('burger-menu__content_visible')
+    }
+
+    if (target.closest('.burger-menu__nav-link')) {
+      burgerMenuNavLinks.forEach(link => link.classList.remove('burger-menu__nav-link_active'))
+      target.classList.add('burger-menu__nav-link_active')
+    }
+  }
+
+  const sliderInit = () => {
+    const slider = document.querySelector('.brands__slider')
+    const swiper = new Swiper('.swiper', {
+      loop: true,
+      speed: 400,
+      slidesPerView: 5,
+      spaceBetween: 20
+    })
+
+  }
+  sliderInit()
+
+  document.addEventListener('click', e => {
+    const target = e.target
+
+    burgerMenuInit(target)
+  })
+})
